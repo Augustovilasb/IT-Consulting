adf12f5e4d8a695638f0369ca5e3343176560c44
Augusto Vilas Boas <augustovilasb@Hotmail.com>
2024-11-01 23:34:53 +0000

    Search and Sort Algorithm added
    Adding the Binary Search algorithm
    Adding the Selection Sort algorithm
    
    As we've learned them in CA_1, they're convenient and easy to use in the CA_2 as well.

--- src/CA_2/SearchService.java
+++ src/CA_2/SearchService.java
@@ -1,4 +1,40 @@
 package CA_2;
 
 public class SearchService {
+
+    public Employee binarySearchEmployee(Employee[] employeeList, String name) {
+
+
+        if (employeeList == null || employeeList.length == 0) {
+            System.out.println("Employee list is empty or null.");
+            return null;
+        }
+
+        if (name == null || name.trim().isEmpty()) {
+            System.out.println("Employee name can't be empty or null.");
+            return null;
+        }
+
+        int low = 0;
+        int high = employeeList.length - 1;
+
+        System.out.println("Searching for: " + name);
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (employeeList[mid].getName().equals(name)) {
+                return employeeList[mid];
+            }
+
+            if (employeeList[mid].getName().trim().equals(name.trim())) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return null;
+
+    }
 }
--- src/CA_2/SortedService.java
+++ src/CA_2/SortedService.java
@@ -1,4 +1,32 @@
 package CA_2;
 
+import javax.xml.namespace.QName;
+
 public class SortedService {
+
+    public void orderEmployeeAscendingSelectionSort(Employee[] employeeList) {
+
+        if (employeeList == null || employeeList.length == 0) {
+            System.out.println("Employee list is empty or null.");
+            return;
+        }
+
+        int length = employeeList.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            int minPosition = i;
+
+            for (int j = i + 1; j < length; j++) {
+                if (employeeList[j].getName().compareTo(employeeList[minPosition].getName()) < 0) {
+                    minPosition = j;
+                }
+            }
+
+            if (minPosition != i) {
+                Employee employeeTemp = employeeList[i];
+                employeeList[i] = employeeList[minPosition];
+                employeeList[minPosition] = employeeTemp;
+            }
+        }
+    }
 }
